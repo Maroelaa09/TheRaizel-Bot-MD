8800b2d2f27e67105d41caff63014549703bece7
zack-omarq <okonda43@gmail.com>
2024-10-21 04:28:19 +0300

    Update menu-azrar.js

--- plugins/menu-azrar.js
+++ plugins/menu-azrar.js
@@ -34,7 +34,7 @@ const handler = async (m, { conn, usedPrefix, __dirname, text, isPrems }) => {
     // إرسال المقطع الصوتي أولاً
     await conn.sendMessage(m.chat, { 
         audio: { 
-            url: 'https://f.uguu.se/NXpKcman.mp3' 
+            url: 'https://f.uguu.se/tUvRXbBz.mp3' 
         }, 
         mimetype: 'audio/mpeg', 
         ptt: true 
